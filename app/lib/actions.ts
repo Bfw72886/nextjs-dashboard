6771b7559aa0a16cffdeafc955f3017cebbcc11f
Brian <Otto72886@m365.bfwbb-lernen.de>
2025-04-09 11:42:25 +0200

    Create update logic for updating invoices (missing db logic)

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -8,32 +8,50 @@ const { Client } = pg;
 const client = new Client();
 
 const FormSchema = z.object({
-    id: z.string(),
-    customerId: z.string(),
-    amount: z.coerce.number(),
-    status: z.enum(['pending', 'paid']),
-    date: z.string(),
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(['pending', 'paid']),
+  date: z.string(),
 });
 
-const CreateInvoice = FormSchema.omit({ id: true, date: true })
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function createInvoice(formData: FormData) {    
-    const { customerId, amount, status } = CreateInvoice.parse({
-        customerId: formData.get('customerId'),
-        amount: formData.get('amount'),
-        status: formData.get('status'),
-    });
+export async function createInvoice(formData: FormData) {
+  const { customerId, amount, status } = CreateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
 
-    const amountInCents = amount * 100;
-    const date = new Date().toISOString().split('T')[0];
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split('T')[0];
 
-    await client.connect();
-    await client.query(`
+  await client.connect();
+  await client.query(`
         INSERT INTO invoices (customer_id, amount, status, date)
         VALUES ('${customerId}', ${amountInCents}, '${status}', '${date}');
     `);
-    await client.end();
+  await client.end();
 
-    revalidatePath('/dashboard/invoices');
-    redirect('/dashboard/invoices');
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
+
+export async function updateInvoice(id: String, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+
+  const amountInCents = amount * 100;
+
+  /*
+  TODO DATABASE LOGIC
+  */
+
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
 }
