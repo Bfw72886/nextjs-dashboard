a073d17e8ce94b3dd06c7d1d8c68e5b31fedffd1
Brian <Otto72886@m365.bfwbb-lernen.de>
2025-04-09 13:45:13 +0200

    Wrap db logic in trycatch

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -5,7 +5,6 @@ import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import pg from 'pg';
 const { Client } = pg;
-const client = new Client();
 
 const FormSchema = z.object({
   id: z.string(),
@@ -28,12 +27,17 @@ export async function createInvoice(formData: FormData) {
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
 
-  await client.connect();
-  await client.query(`
-        INSERT INTO invoices (customer_id, amount, status, date)
-        VALUES ('${customerId}', ${amountInCents}, '${status}', '${date}');
-    `);
-  await client.end();
+  try {
+    const client = new Client();
+    await client.connect();
+    await client.query(`
+          INSERT INTO invoices (customer_id, amount, status, date)
+          VALUES ('${customerId}', ${amountInCents}, '${status}', '${date}');
+      `);
+    await client.end();
+  } catch (error) {
+    console.log(error);
+  }
 
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
@@ -48,25 +52,35 @@ export async function updateInvoice(id: String, formData: FormData) {
 
   const amountInCents = amount * 100;
 
-  await client.connect();
-  await client.query(`
-    UPDATE invoices
-    SET customer_id = '${customerId}', amount = ${amountInCents}, status = '${status}'
-    WHERE id = '${id}'
-    `);
-  await client.end();
+  try {
+    const client = new Client();
+    await client.connect();
+    await client.query(`
+      UPDATE invoices
+      SET customer_id = '${customerId}', amount = ${amountInCents}, status = '${status}'
+      WHERE id = '${id}'
+      `);
+    await client.end();
+  } catch (error) {
+    console.log(error);
+  }
 
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
 
 export async function deleteInvoice(id: string) {
-  await client.connect();
-  await client.query(`
-    DELETE FROM invoices
-    WHERE id = '${id}'
-    `);
-  await client.end();
+  try {
+    const client = new Client();
+    await client.connect();
+    await client.query(`
+      DELETE FROM invoices
+      WHERE id = '${id}'
+      `);
+    await client.end();
+  } catch (error) {
+    console.log(error);
+  }
 
   revalidatePath('/dashboard/invoices');
 }
