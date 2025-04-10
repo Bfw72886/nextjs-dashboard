c789a500832bac59993685fdd287ce5a667741e6
Brian <Otto72886@m365.bfwbb-lernen.de>
2025-04-10 11:23:14 +0200

    Add not-found page and use function notFound() if invoice not found

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,21 +1,26 @@
-import Form from "@/app/ui/invoices/edit-form";
-import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
-import { fetchInvoiceById, fetchCustomers } from "@/app/lib/data";
+import Form from '@/app/ui/invoices/edit-form';
+import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
+import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
+import { notFound } from 'next/navigation';
 
 export default async function Page(props: { params: Promise<{ id: string }> }) {
   const params = await props.params;
   const id = params.id;
-  
+
   const [invoice, customers] = await Promise.all([
     fetchInvoiceById(`'${id}'`),
     fetchCustomers(),
   ]);
-  
+
+  if (!invoice) {
+    notFound();
+  }
+
   return (
     <main>
-      <Breadcrumbs 
+      <Breadcrumbs
         breadcrumbs={[
-          { label: 'Invoices' , href: '/dashboard/invoices' },
+          { label: 'Invoices', href: '/dashboard/invoices' },
           {
             label: 'Edit Invoice',
             href: `/dashboard/invoices/${id}/edit`,
@@ -23,7 +28,7 @@ export default async function Page(props: { params: Promise<{ id: string }> }) {
           },
         ]}
       />
-      <Form invoice={invoice} customers={customers}/>
+      <Form invoice={invoice} customers={customers} />
     </main>
   );
 }
