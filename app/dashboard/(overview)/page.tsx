ef29f6336218cb59cc8442177938b16fab099586
Brian <Otto72886@m365.bfwbb-lernen.de>
2025-04-10 12:38:09 +0200

    Delete unnecessary imports

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -2,18 +2,14 @@ import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import CardWrapper from '@/app/ui/dashboard/cards';
 import { lusitana } from '@/app/ui/fonts';
-import { fetchCardData } from '@/app/lib/data';
 import { Suspense } from 'react';
-import { 
-  RevenueChartSkeleton, 
+import {
+  RevenueChartSkeleton,
   LatestInvoicesSkeleton,
-  CardsSkeleton
+  CardsSkeleton,
 } from '@/app/ui/skeletons';
- 
-export default async function Page() {
-  const cardData = await fetchCardData();
-  const { totalPaidInvoices, totalPendingInvoices, numberOfInvoices, numberOfCustomers } = cardData;
 
+export default async function Page() {
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
@@ -26,10 +22,10 @@ export default async function Page() {
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         <Suspense fallback={<RevenueChartSkeleton />}>
-          { <RevenueChart /> }
+          {<RevenueChart />}
         </Suspense>
         <Suspense fallback={<LatestInvoicesSkeleton />}>
-          { <LatestInvoices /> }
+          {<LatestInvoices />}
         </Suspense>
       </div>
     </main>
